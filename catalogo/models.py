17b73108733f87613afe307ecabd713520b09386
easternpablo <easternpablo@gmail.com>
2019-11-19 23:51:15 +0100

    Proyecto Django 1ª Commit
    Añadir aplicación catalogo con:
    * models.py  Coche y Marca
    * admin.py    register(Coche y Marca)
    * settings.py   añadir app
    * urls.py    añadir carpetas imagenes

---
+++ catalogo/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+class Marca(models.Model):
+    marca_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to="imagenes/Marcas/")
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+class Coche(models.Model):
+    coche_id =  models.AutoField(primary_key=True)
+    marca_id = models.ForeignKey('Marca', on_delete=models.CASCADE)
+    matricula = models.CharField(max_length=7)
+    modelo = models.CharField(max_length=50)
+    kilometros = models.IntegerField(validators=[MinValueValidator(500)])
+    motor = models.CharField(max_length=50) # Ej: Diesel, Gasolina, Electrico,...
+    tipo = models.CharField(max_length=50)  # Ej: Turismo, Monovolumen, TodoTerreno,...
+    caballos = models.IntegerField(validators=[MinValueValidator(60)])
+    cilindradas = models.IntegerField()
+    precio = models.IntegerField(validators=[MinValueValidator(1000)])
+    imagen = models.ImageField(upload_to="imagenes/Coches/")
+
+    def __str__(self):
+        return f"{self.matricula} -> {self.modelo}"
