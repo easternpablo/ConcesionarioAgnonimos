67eb10de5b4394ac0ed6c54f301a03b263d7728d
easternpablo <easternpablo@gmail.com>
2019-11-22 08:36:54 +0100

    commit

--- catalogo/models.py
+++ catalogo/models.py
@@ -2,15 +2,13 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 class Marca(models.Model):
-    marca_id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, primary_key=True)
     imagen = models.ImageField(upload_to="imagenes/Marcas/")
 
     def __str__(self):
         return f"{self.nombre}"
-
 class Coche(models.Model):
-    coche_id =  models.AutoField(primary_key=True)
+    coche_id = models.AutoField(primary_key=True)
     marca_id = models.ForeignKey('Marca', on_delete=models.CASCADE)
     matricula = models.CharField(max_length=7)
     modelo = models.CharField(max_length=50)
