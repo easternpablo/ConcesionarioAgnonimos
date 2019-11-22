b6e3a6db351f2e9eac33934f6073a8c30d5f1ec6
easternpablo <easternpablo@gmail.com>
2019-11-22 08:41:17 +0100

    commit

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-#from .models import Marca
+from .models import Marca
 
 # Vista donde se listan todas las marcas
-# def lista_marcas(request):
-#     marcas = Marca.objects.all()
-#     return render(request, "marcas.html", {"marcas": marcas})
+def lista_marcas(request):
+    marcas = Marca.objects.all()
+    return render(request, "marcas.html", {"marcas": marcas})
