86038f6f30087c987b2a4ea297168167e6ec7946
easternpablo <easternpablo@gmail.com>
2019-11-27 16:56:20 +0100

    commit
    * arreglos

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("logout/", views.logout_vista, name="Logout - Concesionario"),
     path("marcas/", views.lista_marcas, name="Lista de marcas"),
     path("marcas/<marca_id>/", views.lista_coches, name="Lista de coches"), 
-    path("cliente/registro/", views.registro_cliente, name="Registro cliente - Concesionario"),   
+    path("cliente/registro/", views.registro_cliente, name="Registro cliente - Concesionario"),
 ]
